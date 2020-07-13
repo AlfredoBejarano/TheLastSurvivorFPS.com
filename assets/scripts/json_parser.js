4e76130b50c6e81358b023b270e54d87039ae77f
Alfredo <alfredocbejarano@gmail.com>
2020-07-13 18:50:03 -0500

    Add missing MP3 files

--- assets/scripts/json_parser.js
+++ assets/scripts/json_parser.js
@@ -20,18 +20,25 @@ function generateSongList(section, containerId) {
 
 	section.songs.forEach(function(song, index, array) {
 		var itemTag = document.createElement("li")
-
-		var linkTag = document.createElement("a")
-
-		if(containerId == "tab_container") {
+		if(containerId == "tab_container") {		
+			var linkTag = document.createElement("a")
 			linkTag.href = song.tab_url
-		} else {
-			linkTag.href = song.mp3_url
+
+			linkTag.appendChild(document.createTextNode(song.name))
+			itemTag.appendChild(linkTag)		
+		} else {		
+			var audioTag = document.createElement("audio")
+
+			audioTag.controls = true
+			audioTag.autoplay = false
+			audioTag.name = song.name
+			audioTag.src = song.mp3_url
+
+			itemTag.appendChild(document.createTextNode(song.name))
+			itemTag.appendChild(document.createElement("br"))
+			itemTag.appendChild(audioTag)
 		}
 
-		linkTag.appendChild(document.createTextNode(song.name))
-
-		itemTag.appendChild(linkTag)
 		listTag.appendChild(itemTag)
 	})
 
