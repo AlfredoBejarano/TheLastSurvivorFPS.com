2832c3c795e772522639e2832bf65e81b02ea9b2
Alfredo <alfredocbejarano@gmail.com>
2020-07-13 19:48:43 -0500

    Fix song player for mobile devices

--- assets/scripts/audio_player.js
+++ assets/scripts/audio_player.js
@@ -5,5 +5,5 @@ function play(songPath, songName) {
 	audioPlayer.setAttribute("src", songPath)
 	audioPlayer.play()
 
-	audioPlayerName.innerHTML = "Now playing: " + songName
+	audioPlayerName.innerHTML = songName
 }
